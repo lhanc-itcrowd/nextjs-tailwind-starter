5539fa1eaecc0113c4aec0af7eb3571567307171
lhanc-itcrowd <lhanc@itcrowdarg.com>
2023-11-01 09:18:35 -0300

    Add husky

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,22 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
 /* Types */
-import type { ReactNode } from 'react'
-import type { Metadata } from 'next'
+import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
 
 /* Styles */
-import '@lib/styles/globals.css'
+import '@lib/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: 'Next.js + Tailwind Starter | IT Crowd',
-}
+	title: 'Next.js + Tailwind Starter | IT Crowd',
+};
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
